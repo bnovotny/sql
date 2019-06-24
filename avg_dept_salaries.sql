fae0c96c328dbaf16d21be18f474865db71f2f8e
Brenna Kizer <brenna.kizer@spectrumhealth.org>
2019-06-24 15:18:07 -0400

    Removed create view, added aliases

--- avg_dept_salaries.sql
+++ avg_dept_salaries.sql
@@ -1,5 +1,4 @@
 #First practice of queries with employee data
-#Creates view of average salary by department
 
 use employees;
 
@@ -7,14 +6,13 @@ use employees;
 Create a list of the current average salaries for each department,
 ordered from greatest to least
 */
-CREATE VIEW avg_dept_salary AS
-SELECT departments.dept_name, ROUND(AVG(salaries.salary),0) AS avg_salary
-FROM dept_emp
-JOIN departments 
-	ON dept_emp.dept_no = departments.dept_no
-	AND dept_emp.to_date = '9999-01-01'
-JOIN salaries 
-	ON salaries.emp_no = dept_emp.emp_no
-	AND salaries.to_date = '9999-01-01'
-GROUP BY departments.dept_name
-ORDER BY AVG(salaries.salary) DESC;
+SELECT d.dept_name, ROUND(AVG(s.salary),0) AS avg_salary
+FROM dept_emp as de
+JOIN departments as d
+	ON de.dept_no = d.dept_no
+	AND de.to_date = '9999-01-01'
+JOIN salaries as s
+	ON s.emp_no = de.emp_no
+	AND s.to_date = '9999-01-01'
+GROUP BY d.dept_name
+ORDER BY AVG(s.salary) DESC;
